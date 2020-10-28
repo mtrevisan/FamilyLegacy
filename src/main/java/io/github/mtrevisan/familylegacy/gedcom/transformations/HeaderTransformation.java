2364b37ea98cfae77d6606cd63c5a3c0ca1e73eb
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-10-28 16:18:47 +0100

    no message

--- src/main/java/io/github/mtrevisan/familylegacy/gedcom/transformations/HeaderTransformation.java
+++ src/main/java/io/github/mtrevisan/familylegacy/gedcom/transformations/HeaderTransformation.java
@@ -15,14 +15,14 @@ public class HeaderTransformation implements Transformation{
 		final GedcomNode header = moveTag("HEADER", root, "HEAD");
 		final GedcomNode headerSource = moveTag("SOURCE", header, "SOUR");
 		moveTag("VERSION", headerSource, "VERS");
-		moveTag("CORPORATE", headerSource, "CORP");
+		final GedcomNode headerCorporate = moveTag("CORPORATE", headerSource, "CORP");
 		deleteTag(headerSource, "DATA");
-		final GedcomNode sourceCorporatePlace = extractPlace(headerSource, "CORPORATE");
+		final GedcomNode sourceCorporatePlace = extractPlace(headerCorporate);
 		sourceCorporatePlace.withID(Flef.getNextPlaceID(root.getChildrenWithTag("PLACE").size()));
 		root.addChild(sourceCorporatePlace, 1);
 		final GedcomNode sourceCorporatePlacePlaceholder = GedcomNode.create(3, "PLACE")
 			.withID(sourceCorporatePlace.getID());
-		addNode(sourceCorporatePlacePlaceholder, headerSource, "CORPORATE");
+		addNode(sourceCorporatePlacePlaceholder, headerCorporate);
 		deleteTag(header, "DEST");
 		deleteTag(header, "DATE");
 		moveTag("SUBMITTER", header, "SUBM");
@@ -52,33 +52,72 @@ public class HeaderTransformation implements Transformation{
 		final GedcomNode headerSource = moveTag("SOUR", header, "SOURCE");
 		moveTag("VERS", headerSource, "VERSION");
 		final GedcomNode headerCorporate = moveTag("CORP", headerSource, "CORPORATE");
-		final GedcomNode sourceCorporatePlace = extractSubStructure(headerCorporate, "PLACE");
-		if(!sourceCorporatePlace.isEmpty()){
-			GedcomNode place = null;
-			final List<GedcomNode> places = root.getChildrenWithTag("PLACE");
-			for(final GedcomNode p : places)
-				if(p.getID().equals(sourceCorporatePlace.getID())){
-					place = p;
+		final List<GedcomNode> headerCorporatePlaces = headerCorporate.getChildrenWithTag("PLACE");
+		if(headerCorporatePlaces.size() == 1){
+			final GedcomNode headerCorporatePlace = headerCorporatePlaces.get(0);
+			final String headerCorporatePlaceID = headerCorporatePlace.getID();
+			for(final GedcomNode child : root.getChildrenWithTag("PLACE"))
+				if(headerCorporatePlaceID.equals(child.getID())){
+					final GedcomNode addr = GedcomNode.create(3, "ADDR");
+					List<GedcomNode> components = child.getChildrenWithTag("STREET");
+					if(components.size() == 1)
+						addr.withValue(components.get(0).getValue());
+					components = child.getChildrenWithTag("CITY");
+					if(components.size() == 1)
+						addr.addChild(GedcomNode.create(4, "CITY")
+							.withValue(components.get(0).getValue()));
+					components = child.getChildrenWithTag("STATE");
+					if(components.size() == 1)
+						addr.addChild(GedcomNode.create(4, "STAE")
+							.withValue(components.get(0).getValue()));
+					components = child.getChildrenWithTag("POSTAL_CODE");
+					if(components.size() == 1)
+						addr.addChild(GedcomNode.create(4, "POST")
+							.withValue(components.get(0).getValue()));
+					components = child.getChildrenWithTag("COUNTRY");
+					if(components.size() == 1)
+						addr.addChild(GedcomNode.create(4, "CTRY")
+							.withValue(components.get(0).getValue()));
+
+					headerCorporate.removeChild(headerCorporatePlace);
+					headerCorporate.addChild(addr);
+
+					transferValues(child, "PHONE", headerCorporate, "PHON", 1);
+					transferValues(child, "FAX", headerCorporate, "FAX", 1);
+					transferValues(child, "EMAIL", headerCorporate, "EMAIL", 1);
+					transferValues(child, "WWW", headerCorporate, "WWW", 1);
+
+					root.removeChild(child);
+
 					break;
 				}
-			if(place == null)
-				throw new IllegalArgumentException("Cannot find place with ID " + sourceCorporatePlace.getID());
-
-			place.removeID();
-			deleteTag(headerCorporate, "PLACE");
-			headerCorporate.addChild(place);
 		}
 		moveTag("SUBM", header, "SUBMITTER");
 		moveTag("COPR", header, "COPYRIGHT");
 		deleteTag(header, "PROTOCOL_VERSION");
-		final GedcomNode headerGedcom = GedcomNode.create(1, "GEDC");
-		final GedcomNode headerGedcomProtocolVersion = GedcomNode.create(2, "VERS");
-		headerGedcomProtocolVersion.withValue("5.5.1");
-		headerGedcom.addChild(headerGedcomProtocolVersion);
-		addNode(headerGedcom, header);
-		transferValues(header, "CHARSET", header, "CHAR", 1);
-		//TODO
-		splitNote(header, "NOTE");
+		final GedcomNode gedcom = GedcomNode.create(1, "GEDC");
+		gedcom.addChild(GedcomNode.create(2, "VERS")
+			.withValue("5.5.1"));
+		gedcom.addChild(GedcomNode.create(2, "FORM")
+			.withValue("LINEAGE-LINKED"));
+		addNode(gedcom, header);
+		moveTag("CHAR", header, "CHARSET");
+		final List<GedcomNode> headerNotes = header.getChildrenWithTag("NOTE");
+		if(headerNotes.size() == 1){
+			final GedcomNode headerNote = headerNotes.get(0);
+			final String headerNoteID = headerNote.getID();
+			for(final GedcomNode child : root.getChildrenWithTag("NOTE"))
+				if(headerNoteID.equals(child.getID())){
+					final GedcomNode addr = GedcomNode.create(1, "NOTE")
+						.withValue(child.getValue());
+
+					header.removeChild(headerNote);
+					header.addChild(addr);
+					root.removeChild(child);
+
+					break;
+				}
+		}
 	}
 
 }
