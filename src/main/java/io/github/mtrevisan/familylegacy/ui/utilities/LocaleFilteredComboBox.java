75c53398c90cb6a5d1df63742cbc34371b0fcf58
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-12-02 09:31:57 +0100

    modified the locale combobox

--- src/main/java/io/github/mtrevisan/familylegacy/ui/utilities/LocaleFilteredComboBox.java
+++ src/main/java/io/github/mtrevisan/familylegacy/ui/utilities/LocaleFilteredComboBox.java
@@ -25,8 +25,6 @@
 package io.github.mtrevisan.familylegacy.ui.utilities;
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicComboBoxEditor;
-import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.text.DateFormat;
@@ -37,17 +35,30 @@ import java.util.Comparator;
 import java.util.Locale;
 
 
-public class LocaleFilteredComboBox extends JComboBox<Locale>{
+public class LocaleFilteredComboBox extends JComboBox<LocaleFilteredComboBox.MyLocale>{
 
-	private static final Locale[] LOCALE_ITEMS;
+	private static final MyLocale[] LOCALE_ITEMS;
 	static{
 		final Locale[] availableLocales = DateFormat.getAvailableLocales();
-		final Collection<Locale> list = new ArrayList<>(availableLocales.length);
+		final Collection<MyLocale> list = new ArrayList<>(availableLocales.length);
 		for(final Locale availableLocale : availableLocales)
 			if(availableLocale.toLanguageTag() != null)
-				list.add(availableLocale);
-		LOCALE_ITEMS = list.toArray(Locale[]::new);
-		Arrays.sort(LOCALE_ITEMS, Comparator.comparing(Locale::getDisplayName));
+				list.add(new MyLocale(availableLocale));
+		LOCALE_ITEMS = list.toArray(MyLocale[]::new);
+		Arrays.sort(LOCALE_ITEMS, Comparator.comparing(ml -> ml.toString().toLowerCase(Locale.ROOT)));
+	}
+
+	protected static final class MyLocale{
+		private final Locale locale;
+
+		MyLocale(final Locale locale){
+			this.locale = locale;
+		}
+
+		@Override
+		public String toString(){
+			return locale.getDisplayName();
+		}
 	}
 
 
@@ -58,8 +69,6 @@ public class LocaleFilteredComboBox extends JComboBox<Locale>{
 		super(LOCALE_ITEMS);
 
 		setEditable(true);
-		setRenderer(new LocaleListCellRenderer());
-		setEditor(new LocaleComboBoxEditor());
 
 		setFixedWidth();
 
@@ -75,9 +84,9 @@ public class LocaleFilteredComboBox extends JComboBox<Locale>{
 	/** Fix size of combobox to maximum size required to show the longest name. */
 	private void setFixedWidth(){
 		double maxWidth = 0.;
-		Locale maxLocale = Locale.US;
-		for(final Locale locale : LOCALE_ITEMS){
-			final double w = FontHelper.getStringBounds(getFont(), locale.getDisplayName()).getWidth();
+		MyLocale maxLocale = new MyLocale(Locale.US);
+		for(final MyLocale locale : LOCALE_ITEMS){
+			final double w = FontHelper.getStringBounds(getFont(), locale.toString()).getWidth();
 			if(w > maxWidth){
 				maxWidth = w;
 				maxLocale = locale;
@@ -91,13 +100,13 @@ public class LocaleFilteredComboBox extends JComboBox<Locale>{
 			showPopup();
 
 		//filter
-		final Collection<Locale> filterArray = new ArrayList<>();
-		for(final Locale locale : LOCALE_ITEMS)
-			if(locale.getDisplayName().toLowerCase(Locale.ROOT).contains(enteredText.toLowerCase(Locale.ROOT)))
+		final Collection<MyLocale> filterArray = new ArrayList<>();
+		for(final MyLocale locale : LOCALE_ITEMS)
+			if(locale.toString().toLowerCase(Locale.ROOT).contains(enteredText.toLowerCase(Locale.ROOT)))
 				filterArray.add(locale);
 
 		if(!filterArray.isEmpty()){
-			final DefaultComboBoxModel<Locale> model = (DefaultComboBoxModel<Locale>)getModel();
+			final DefaultComboBoxModel<MyLocale> model = (DefaultComboBoxModel<MyLocale>)getModel();
 			model.removeAllElements();
 			model.addAll(filterArray);
 
@@ -107,59 +116,16 @@ public class LocaleFilteredComboBox extends JComboBox<Locale>{
 
 	public void setSelectedByLanguageTag(String languageTag){
 		languageTag = languageTag.toLowerCase(Locale.ROOT);
-		final DefaultComboBoxModel<Locale> model = (DefaultComboBoxModel<Locale>)getModel();
+		MyLocale locale = new MyLocale(Locale.ROOT);
+		final DefaultComboBoxModel<MyLocale> model = (DefaultComboBoxModel<MyLocale>)getModel();
 		for(int index = 0; index < model.getSize(); index ++){
-			final Locale locale = model.getElementAt(index);
-			if(locale.toLanguageTag().toLowerCase(Locale.ROOT).equals(languageTag)){
-				setSelectedItem(locale);
+			final MyLocale loc = model.getElementAt(index);
+			if(loc.toString().toLowerCase(Locale.ROOT).equals(languageTag)){
+				locale = loc;
 				break;
 			}
 		}
-	}
-
-
-	private static class LocaleListCellRenderer extends DefaultListCellRenderer{
-		@Override
-		public Component getListCellRendererComponent(final JList<?> list, final Object value, final int index, final boolean isSelected,
-				final boolean cellHasFocus){
-			super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-
-			setText(((Locale)value).getDisplayName());
-			return this;
-		}
-	}
-
-	private static class LocaleComboBoxEditor extends BasicComboBoxEditor{
-		private final JLabel label = new JLabel();
-		private final JTextField editorComponent = new JTextField();
-		private Object selectedItem;
-
-
-		LocaleComboBoxEditor(){
-			label.setOpaque(false);
-			label.setBorder(null);
-
-			editorComponent.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 2));
-			editorComponent.setBorder(null);
-			editorComponent.add(label);
-		}
-
-		@Override
-		public Component getEditorComponent(){
-			return editorComponent;
-		}
-
-		@Override
-		public Object getItem(){
-			return selectedItem;
-		}
-
-		@Override
-		public void setItem(final Object item){
-			selectedItem = item;
-			label.setText(((Locale)item).getDisplayName());
-		}
-
+		setSelectedItem(locale);
 	}
 
 }
