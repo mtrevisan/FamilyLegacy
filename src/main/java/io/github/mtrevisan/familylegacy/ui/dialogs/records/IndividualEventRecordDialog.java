a08f22629ef9dc82ec6b294b6761b68c490d3a0b
Mauro Trevisan <mauro.simioni@targaubiest.com>
2022-10-14 13:04:33 +0200

    added all dialogs that have to be implemented

---
+++ src/main/java/io/github/mtrevisan/familylegacy/ui/dialogs/records/IndividualEventRecordDialog.java
@@ -0,0 +1,296 @@
+/**
+ * Copyright (c) 2020-2022 Mauro Trevisan
+ * <p>
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.mtrevisan.familylegacy.ui.dialogs.records;
+
+import io.github.mtrevisan.familylegacy.gedcom.Flef;
+import io.github.mtrevisan.familylegacy.gedcom.Gedcom;
+import io.github.mtrevisan.familylegacy.gedcom.GedcomGrammarParseException;
+import io.github.mtrevisan.familylegacy.gedcom.GedcomNode;
+import io.github.mtrevisan.familylegacy.gedcom.GedcomParseException;
+import io.github.mtrevisan.familylegacy.gedcom.Store;
+import io.github.mtrevisan.familylegacy.gedcom.events.EditEvent;
+import io.github.mtrevisan.familylegacy.services.ResourceHelper;
+import io.github.mtrevisan.familylegacy.ui.dialogs.citations.NoteCitationDialog;
+import io.github.mtrevisan.familylegacy.ui.panels.IndividualPanel;
+import io.github.mtrevisan.familylegacy.ui.utilities.ScaledImage;
+import io.github.mtrevisan.familylegacy.ui.utilities.eventbus.EventBusService;
+import net.miginfocom.swing.MigLayout;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Frame;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serial;
+import java.util.StringJoiner;
+
+
+//TODO
+public class IndividualEventRecordDialog extends JDialog{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(IndividualEventRecordDialog.class);
+
+	@Serial
+	private static final long serialVersionUID = 2075397360104239479L;
+
+	private static final String NAMES_SEPARATOR = ", ";
+	private static final String NO_DATA = "?";
+
+	private static final int PARTNER_IMAGE_MINIMUM_WIDTH = 30;
+	private static final int PARTNER_IMAGE_MINIMUM_HEIGHT = 38;
+
+	private static final Font FONT_PRIMARY = new Font("Tahoma", Font.BOLD, 11);
+
+	private static final DefaultComboBoxModel<String> TYPE_MODEL = new DefaultComboBoxModel<>(new String[]{StringUtils.EMPTY, "unknown", "marriage", "not married", "civil marriage", "religious marriage", "common law marriage", "partnership", "registered partnership", "living together", "living apart together"});
+	private static final DefaultComboBoxModel<String> RESTRICTION_MODEL = new DefaultComboBoxModel<>(new String[]{StringUtils.EMPTY, "confidential", "locked", "private"});
+
+	private static final ImageIcon ICON_NOTE = ResourceHelper.getImage("/images/note.png", 20, 20);
+
+	private final JLabel individualLabel = new JLabel("Indivisdual:");
+	private final ScaledImage individualImage = new ScaledImage(null);
+	private final JLabel individualName = new JLabel(StringUtils.EMPTY);
+	private final JButton individualNoteButton = new JButton(StringUtils.EMPTY);
+	private final JButton eventButton = new JButton("Events");
+	private final JButton groupButton = new JButton("Groups");
+	private final JButton culturalNormButton = new JButton("Cultural norms");
+	private final JButton noteButton = new JButton("Notes");
+	private final JButton sourceButton = new JButton("Sources");
+	private final JLabel restrictionLabel = new JLabel("Restriction:");
+	private final JComboBox<String> restrictionComboBox = new JComboBox<>(RESTRICTION_MODEL);
+
+	private GedcomNode individual;
+	private final Flef store;
+
+
+	public IndividualEventRecordDialog(final GedcomNode individual, final Flef store, final Frame parent){
+		super(parent, true);
+
+		this.individual = individual;
+		this.store = store;
+
+		initComponents();
+
+		loadData();
+	}
+
+	private void initComponents(){
+		individualLabel.setFont(FONT_PRIMARY);
+		individualLabel.setLabelFor(individualName);
+		individualNoteButton.setIcon(ICON_NOTE);
+		individualNoteButton.setToolTipText("Add note to parent 1");
+		individualNoteButton.addActionListener(evt -> {
+			final Frame parent = (Frame)getParent();
+			final NoteCitationDialog noteCitationDialog = NoteCitationDialog.createNoteCitation(store, parent);
+			//TODO onCloseGracefully
+			if(!noteCitationDialog.loadData(individual, null))
+				noteCitationDialog.addAction();
+
+			noteCitationDialog.setSize(450, 260);
+			noteCitationDialog.setLocationRelativeTo(parent);
+			noteCitationDialog.setVisible(true);
+		});
+
+		eventButton.addActionListener(e -> {
+			//TODO
+		});
+
+		groupButton.addActionListener(e -> EventBusService.publish(new EditEvent(EditEvent.EditType.GROUP_CITATION, individual)));
+
+		culturalNormButton.addActionListener(e -> {
+			//TODO
+		});
+
+		noteButton.addActionListener(e -> EventBusService.publish(new EditEvent(EditEvent.EditType.NOTE_CITATION, individual)));
+
+		sourceButton.addActionListener(e -> EventBusService.publish(new EditEvent(EditEvent.EditType.SOURCE_CITATION, individual)));
+
+		restrictionLabel.setLabelFor(restrictionComboBox);
+		restrictionComboBox.setEditable(true);
+		restrictionComboBox.addActionListener(e -> {
+			if("comboBoxEdited".equals(e.getActionCommand())){
+				final String newValue = (String)RESTRICTION_MODEL.getSelectedItem();
+				RESTRICTION_MODEL.addElement(newValue);
+
+				restrictionComboBox.setSelectedItem(newValue);
+			}
+		});
+		restrictionComboBox.setSelectedIndex(0);
+
+
+		final JTabbedPane tabbedPane = new JTabbedPane();
+
+		final JPanel panelMembers = new JPanel(new MigLayout("debug", "[fill][][]"));
+		panelMembers.add(individualLabel, "span 3,wrap");
+		panelMembers.add(individualImage);
+		panelMembers.add(individualName, "grow");
+		panelMembers.add(individualNoteButton, "top");
+
+		final JPanel panelEvents = new JPanel(new MigLayout());
+		panelEvents.add(eventButton, "sizegroup button,grow,wrap");
+
+		final JPanel panelGroups = new JPanel(new MigLayout());
+		panelGroups.add(groupButton, "sizegroup button,grow,wrap");
+
+		final JPanel panelCulturalNorms = new JPanel(new MigLayout());
+		panelCulturalNorms.add(culturalNormButton, "sizegroup button,grow,wrap");
+
+		final JPanel panelNotes = new JPanel(new MigLayout());
+		panelNotes.add(noteButton, "sizegroup button,grow,wrap");
+
+		final JPanel panelSources = new JPanel(new MigLayout());
+		panelSources.add(sourceButton, "sizegroup button,grow,wrap");
+
+		final JPanel panelGeneral = new JPanel(new MigLayout());
+		panelGeneral.add(restrictionLabel, "align label,split 2");
+		panelGeneral.add(restrictionComboBox, "grow");
+
+		tabbedPane.add("Members", panelMembers);
+		tabbedPane.add("Events", panelEvents);
+		tabbedPane.add("Groups", panelGroups);
+		tabbedPane.add("Cultural rules", panelCulturalNorms);
+		tabbedPane.add("Notes", panelNotes);
+		tabbedPane.add("Sources", panelSources);
+		tabbedPane.add("General", panelGeneral);
+
+		setLayout(new MigLayout());
+		add(tabbedPane, "grow,wrap");
+	}
+
+	public final void loadData(final GedcomNode individual){
+		this.individual = individual;
+
+		loadData();
+
+		repaint();
+	}
+
+	private void loadData(){
+		individualNoteButton.setEnabled(!individual.isEmpty());
+		loadPartnerData(individual, individualImage, individualName, individualNoteButton);
+
+		//TODO
+
+		restrictionComboBox.setSelectedItem(store.traverse(individual, "RESTRICTION").getValue());
+	}
+
+	private void loadPartnerData(final GedcomNode partner, final ScaledImage partnerImage, final JLabel partnerName, final JButton partnerNotes){
+		if(!partner.isEmpty()){
+			GedcomNode preferredImage = store.traverse(partner, "PREFERRED_IMAGE");
+			final String partnerPreferredImageXRef = preferredImage.getValue();
+			//top-left and bottom-right
+			final String partnerPreferredImageCropCoordinates = store.traverse(preferredImage, "CROP")
+				.getValue();
+			try{
+				preferredImage = store.getSource(partnerPreferredImageXRef);
+				final String partnerPreferredImagePath = store.traverse(preferredImage, "FILE")
+					.getValue();
+				partnerImage.setImage(ResourceHelper.readImage(new File(store.getBasePath(), partnerPreferredImagePath)));
+				partnerImage.setMinimumSize(new Dimension(PARTNER_IMAGE_MINIMUM_WIDTH, PARTNER_IMAGE_MINIMUM_HEIGHT));
+				partnerImage.setEnabled(true);
+
+				if(StringUtils.isNotBlank(partnerPreferredImageCropCoordinates)){
+					final String[] coords = StringUtils.split(partnerPreferredImageCropCoordinates, ' ');
+					final int startX = Integer.parseInt(coords[0]);
+					final int startY = Integer.parseInt(coords[1]);
+					final int endX = Integer.parseInt(coords[2]);
+					final int endY = Integer.parseInt(coords[3]);
+//					partnerImage.setWindow(startX, startY, endX, endY);
+					partnerImage.setWindow(190, 120, 500, 500);
+				}
+			}
+			catch(final IOException e){
+				LOGGER.error("Cannot load preferred image of individual {}", partner.getID(), e);
+
+				partnerImage.setEnabled(false);
+			}
+			partnerName.setEnabled(true);
+			partnerName.setText(getIndividualText(partner));
+			partnerNotes.setEnabled(true);
+		}
+		else{
+			partnerImage.setEnabled(false);
+			partnerImage.setImage(null);
+			partnerName.setText(null);
+			partnerName.setEnabled(false);
+			partnerNotes.setEnabled(false);
+		}
+	}
+
+	private String getIndividualText(final GedcomNode partner){
+		final StringJoiner text = new StringJoiner(StringUtils.SPACE);
+		text.add(partner.getID() + ":");
+		text.add(IndividualPanel.extractFirstCompleteName(partner, NAMES_SEPARATOR, store));
+		final String birthYear = IndividualPanel.extractBirthYear(partner, store);
+		final String deathYear = IndividualPanel.extractDeathYear(partner, store);
+		text.add("(" + (StringUtils.isNotBlank(birthYear)? birthYear: NO_DATA) + "–"
+			+ (StringUtils.isNotBlank(deathYear)? deathYear: NO_DATA) + ")");
+		return text.toString();
+	}
+
+
+	public static void main(final String[] args) throws GedcomParseException, GedcomGrammarParseException{
+		try{
+			final String lookAndFeelName = UIManager.getSystemLookAndFeelClassName();
+			UIManager.setLookAndFeel(lookAndFeelName);
+		}
+		catch(final Exception ignored){}
+
+		final Store storeGedcom = new Gedcom();
+		final Flef storeFlef = (Flef)storeGedcom.load("/gedg/gedcom_5.5.1.tcgb.gedg", "src/main/resources/ged/large.ged")
+			.transform();
+//		final GedcomNode individual = storeFlef.getIndividuals().get(0);
+		final GedcomNode individual = storeFlef.getIndividual("I1");
+
+		EventQueue.invokeLater(() -> {
+			final IndividualEventRecordDialog dialog = new IndividualEventRecordDialog(individual, storeFlef, new JFrame());
+			dialog.setTitle("Individual record");
+
+			dialog.addWindowListener(new java.awt.event.WindowAdapter(){
+				@Override
+				public void windowClosing(final java.awt.event.WindowEvent e){
+					System.exit(0);
+				}
+			});
+			dialog.setSize(400, 500);
+			dialog.setLocationRelativeTo(null);
+			dialog.setVisible(true);
+		});
+	}
+
+}
---
+++ src/main/java/io/github/mtrevisan/familylegacy/ui/dialogs/records/ResearchStatusRecordDialog.java
@@ -0,0 +1,330 @@
+/**
+ * Copyright (c) 2022 Mauro Trevisan
+ * <p>
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.mtrevisan.familylegacy.ui.dialogs.records;
+
+import io.github.mtrevisan.familylegacy.gedcom.Flef;
+import io.github.mtrevisan.familylegacy.gedcom.GedcomGrammarParseException;
+import io.github.mtrevisan.familylegacy.gedcom.GedcomNode;
+import io.github.mtrevisan.familylegacy.gedcom.GedcomParseException;
+import io.github.mtrevisan.familylegacy.gedcom.events.EditEvent;
+import io.github.mtrevisan.familylegacy.services.ResourceHelper;
+import io.github.mtrevisan.familylegacy.ui.dialogs.citations.CulturalNormCitationDialog;
+import io.github.mtrevisan.familylegacy.ui.dialogs.citations.NoteCitationDialog;
+import io.github.mtrevisan.familylegacy.ui.dialogs.citations.SourceCitationDialog;
+import io.github.mtrevisan.familylegacy.ui.utilities.eventbus.EventBusService;
+import io.github.mtrevisan.familylegacy.ui.utilities.eventbus.EventHandler;
+import net.miginfocom.swing.MigLayout;
+import org.apache.commons.lang3.StringUtils;
+import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.Serial;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.function.Consumer;
+
+
+//TODO
+public class ResearchStatusRecordDialog extends JDialog{
+
+	@Serial
+	private static final long serialVersionUID = 4728999064397477461L;
+
+	private static final KeyStroke ESCAPE_STROKE = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+
+	private static final DefaultComboBoxModel<String> TYPE_MODEL = new DefaultComboBoxModel<>(new String[]{"gregorian", "julian", "islamic", "hebrew", "chinese", "indian", "buddhist",
+		"french-republican", "coptic", "soviet eternal", "ethiopian", "mayan"});
+
+	private static final ImageIcon ICON_CULTURAL_NORM = ResourceHelper.getImage("/images/culturalNorm.png", 20, 20);
+	private static final ImageIcon ICON_NOTE = ResourceHelper.getImage("/images/note.png", 20, 20);
+	private static final ImageIcon ICON_SOURCE = ResourceHelper.getImage("/images/source.png", 20, 20);
+
+/*
+  +1 CREATION_DATE    {1:1}
+    +2 DATE <CREATION_DATE>    {1:1}
+  +1 CHANGE_DATE    {0:M}
+    +2 DATE <CHANGE_DATE>    {1:1}
+    +2 NOTE @<XREF:NOTE>@    {0:1}
+*/
+	//TODO mandatory
+	private final JLabel typeLabel = new JLabel("Type:");
+	private final JComboBox<String> typeComboBox = new JComboBox<>(TYPE_MODEL);
+	//TODO 0 to M
+	private final JButton culturalNormButton = new JButton(ICON_CULTURAL_NORM);
+	//TODO 0 to M
+	private final JButton noteButton = new JButton(ICON_NOTE);
+	//TODO 0 to M
+	private final JButton sourceButton = new JButton(ICON_SOURCE);
+	private final JButton helpButton = new JButton("Help");
+	private final JButton okButton = new JButton("Ok");
+	private final JButton cancelButton = new JButton("Cancel");
+
+	private GedcomNode container;
+	private long originalCulturalNormsHash;
+	private long originalNotesHash;
+	private long originalSourcesHash;
+
+	private Consumer<Object> onAccept;
+	private final Flef store;
+
+
+	public ResearchStatusRecordDialog(final Flef store, final Frame parent){
+		super(parent, true);
+
+		this.store = store;
+
+		initComponents();
+	}
+
+	private void initComponents(){
+		typeLabel.setLabelFor(typeComboBox);
+		AutoCompleteDecorator.decorate(typeComboBox);
+
+		final Border originalButtonBorder = okButton.getBorder();
+		noteButton.setToolTipText("Add note");
+		culturalNormButton.addActionListener(evt -> {
+			final GedcomNode newCulturalNorm = store.create("CULTURAL_NORM");
+
+			final Consumer<Object> onCloseGracefully = ignored -> {
+				//if ok was pressed, add this note to the parent container
+				final String newCulturalNormID = store.addCulturalNorm(newCulturalNorm);
+				container.addChildReference("CULTURAL_NORM", newCulturalNormID);
+
+				culturalNormButton.setBorder(calculateCulturalNormsHashCode() != originalCulturalNormsHash
+					? new LineBorder(Color.BLUE)
+					: originalButtonBorder);
+
+				//put focus on the ok button
+				okButton.grabFocus();
+			};
+
+			//fire add event
+			EventBusService.publish(new EditEvent(EditEvent.EditType.CULTURAL_NORM_CITATION, container, onCloseGracefully));
+		});
+
+		noteButton.setToolTipText("Add note");
+		noteButton.addActionListener(evt -> {
+			final Consumer<Object> onAccept = ignored -> {
+				noteButton.setBorder(calculateNotesHashCode() != originalNotesHash
+					? new LineBorder(Color.BLUE)
+					: originalButtonBorder);
+
+				//put focus on the ok button
+				okButton.grabFocus();
+			};
+
+			EventBusService.publish(new EditEvent(EditEvent.EditType.NOTE_CITATION, container, onAccept));
+		});
+
+		sourceButton.setToolTipText("Add source");
+		sourceButton.addActionListener(evt -> {
+			final Consumer<Object> onAccept = ignored -> {
+				sourceButton.setBorder(calculateSourcesHashCode() != originalSourcesHash
+					? new LineBorder(Color.BLUE)
+					: originalButtonBorder);
+
+				//put focus on the ok button
+				okButton.grabFocus();
+			};
+
+			EventBusService.publish(new EditEvent(EditEvent.EditType.SOURCE_CITATION, container, onAccept));
+		});
+
+		final ActionListener okAction = evt -> okAction();
+		final ActionListener cancelAction = evt -> setVisible(false);
+		//TODO link to help
+//		helpButton.addActionListener(evt -> dispose());
+		okButton.setEnabled(false);
+		okButton.addActionListener(okAction);
+		cancelButton.addActionListener(cancelAction);
+		getRootPane().registerKeyboardAction(cancelAction, ESCAPE_STROKE, JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+
+		setLayout(new MigLayout(StringUtils.EMPTY, "[grow]"));
+//		setLayout(new MigLayout("debug", "[grow]"));
+		add(typeLabel, "align label,split 2");
+		add(typeComboBox, "grow,wrap");
+		add(culturalNormButton, "split 3,sizegroup button,center");
+		add(noteButton, "sizegroup button,center");
+		add(sourceButton, "sizegroup button,center,wrap paragraph");
+		add(helpButton, "tag help2,split 3,sizegroup button2");
+		add(okButton, "tag ok,sizegroup button2");
+		add(cancelButton, "tag cancel,sizegroup button2");
+	}
+
+	private int calculateCulturalNormsHashCode(){
+		return store.traverseAsList(container, "CULTURAL_NORM[]").hashCode();
+	}
+
+	private int calculateNotesHashCode(){
+		return store.traverseAsList(container, "NOTE[]").hashCode();
+	}
+
+	private int calculateSourcesHashCode(){
+		return store.traverseAsList(container, "SOURCE[]").hashCode();
+	}
+
+	private boolean sourceContainsEvent(final String event){
+		boolean containsEvent = false;
+		final List<GedcomNode> events = store.traverseAsList(container, "EVENT[]");
+		for(int i = 0; !containsEvent && i < events.size(); i ++)
+			if(events.get(i).getValue().equalsIgnoreCase(event))
+				containsEvent = true;
+		return containsEvent;
+	}
+
+	private void okAction(){
+		final String type = (String)typeComboBox.getSelectedItem();
+
+		container.replaceChildValue("TYPE", type);
+		//TODO
+	}
+
+	public final void loadData(final GedcomNode calendar, final Consumer<Object> onAccept){
+		this.container = calendar;
+		this.onAccept = onAccept;
+
+		originalCulturalNormsHash = calculateCulturalNormsHashCode();
+		originalNotesHash = calculateNotesHashCode();
+		originalSourcesHash = calculateSourcesHashCode();
+
+		final StringJoiner events = new StringJoiner(", ");
+		for(final GedcomNode event : store.traverseAsList(calendar, "EVENT[]"))
+			events.add(event.getValue());
+		final String type = store.traverse(calendar, "TYPE").getValue();
+		final String author = store.traverse(calendar, "AUTHOR").getValue();
+		final String publicationFacts = store.traverse(calendar, "PUBLICATION_FACTS").getValue();
+		final GedcomNode dateNode = store.traverse(calendar, "DATE");
+		//TODO
+		final GedcomNode place = store.traverse(calendar, "PLACE");
+		final GedcomNode placeCertainty = store.traverse(calendar, "PLACE.CERTAINTY");
+		final GedcomNode placeCredibility = store.traverse(calendar, "PLACE.CREDIBILITY");
+		final String mediaType = store.traverse(calendar, "MEDIA_TYPE").getValue();
+
+		typeComboBox.setSelectedItem(type);
+		sourceButton.setEnabled(true);
+		noteButton.setEnabled(true);
+	}
+
+
+	public static void main(final String[] args) throws GedcomParseException, GedcomGrammarParseException{
+		try{
+			final String lookAndFeelName = UIManager.getSystemLookAndFeelClassName();
+			UIManager.setLookAndFeel(lookAndFeelName);
+		}
+		catch(final Exception ignored){}
+
+		final Flef store = new Flef();
+		store.load("/gedg/flef_0.0.8.gedg", "src/main/resources/ged/small.flef.ged")
+			.transform();
+		final GedcomNode calendar = store.getCalendars().get(0);
+
+		EventQueue.invokeLater(() -> {
+			final JFrame parent = new JFrame();
+			final Object listener = new Object(){
+				@EventHandler
+				public void refresh(final EditEvent editCommand){
+					switch(editCommand.getType()){
+						case CULTURAL_NORM_CITATION -> {
+							final CulturalNormCitationDialog dialog = new CulturalNormCitationDialog(store, parent);
+							final GedcomNode culturalNormCitation = editCommand.getContainer();
+							dialog.setTitle(culturalNormCitation.getID() != null
+								? "Cultural norm citation " + culturalNormCitation.getID() + " for calendar " + calendar.getID()
+								: "New cultural norm citation for calendar " + calendar.getID());
+							if(!dialog.loadData(editCommand.getContainer(), editCommand.getOnCloseGracefully()))
+								//show a cultural norm input dialog
+								dialog.addAction();
+
+							dialog.setSize(450, 260);
+							dialog.setLocationRelativeTo(parent);
+							dialog.setVisible(true);
+						}
+						case NOTE_CITATION -> {
+							final NoteCitationDialog dialog = NoteCitationDialog.createNoteCitation(store, parent);
+							final GedcomNode noteCitation = editCommand.getContainer();
+							dialog.setTitle(noteCitation.getID() != null
+								? "Note citation " + noteCitation.getID() + " for calendar " + calendar.getID()
+								: "New note citation for calendar " + calendar.getID());
+							if(!dialog.loadData(editCommand.getContainer(), editCommand.getOnCloseGracefully()))
+								//show a note input dialog
+								dialog.addAction();
+
+							dialog.setSize(450, 260);
+							dialog.setLocationRelativeTo(parent);
+							dialog.setVisible(true);
+						}
+						case SOURCE_CITATION -> {
+							final SourceCitationDialog dialog = new SourceCitationDialog(store, parent);
+							final GedcomNode sourceCitation = editCommand.getContainer();
+							dialog.setTitle(sourceCitation.getID() != null
+								? "Source citation " + sourceCitation.getID() + " for calendar " + calendar.getID()
+								: "New source citation for calendar " + calendar.getID());
+							if(!dialog.loadData(editCommand.getContainer(), editCommand.getOnCloseGracefully()))
+								dialog.addAction();
+
+							dialog.setSize(550, 650);
+							dialog.setLocationRelativeTo(parent);
+							dialog.setVisible(true);
+						}
+					}
+				}
+			};
+			EventBusService.subscribe(listener);
+
+			final ResearchStatusRecordDialog dialog = new ResearchStatusRecordDialog(store, parent);
+			dialog.setTitle(calendar.getID() != null? "Calendar " + calendar.getID(): "New Calendar");
+			dialog.loadData(calendar, null);
+
+			dialog.addWindowListener(new WindowAdapter(){
+				@Override
+				public void windowClosing(final WindowEvent e){
+					System.exit(0);
+				}
+			});
+			dialog.setSize(300, 155);
+			dialog.setLocationRelativeTo(null);
+			dialog.setVisible(true);
+		});
+	}
+
+}
