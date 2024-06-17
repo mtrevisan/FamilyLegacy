170fb06ccd5155dc43a5e930b7860c28a1c489a0
Mauro Trevisan <mauro.simioni@targatelematics.com>
2024-06-17 17:31:39 +0200

    Refactor SQL schema and update GEDCOM parsing comments
    Mainly, SQL schema was revamped to simplify table definitions and to improve readability. PRIMARY KEY constraints were embedded directly with column definitions, and redundant separate definitions were removed. Moreover, 'unique' constraint was added to some 'identifier' fields. In the GEDCOM definitions, comments were updated to provide accurate information. The "TYPE" field's comment was modified to eliminate the unnecessary uniqueness specification.

--- src/main/resources/gedg/treebard/FLeF.sql
+++ src/main/resources/gedg/treebard/FLeF.sql
@@ -2,41 +2,38 @@
 
 CREATE TABLE ASSERTION
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  CITATION_ID     numeric NOT NULL,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
  ROLE            text NULL,
  CERTAINTY       text NULL,
  CREDIBILITY     numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (CITATION_ID) REFERENCES CITATION ( "ID" )
 );
 -- REFERENCE: "place", "cultural norm", "date", "repository", "person", "group", "media", "person name"
 
 CREATE TABLE CITATION
 (
- "ID"         numeric NOT NULL,
+ "ID"         numeric PRIMARY KEY,
  SOURCE_ID    numeric NOT NULL,
  LOCATION     text NULL,
  EXTRACT_ID   numeric NULL,
  EXTRACT_TYPE text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (SOURCE_ID) REFERENCES SOURCE ( "ID" ),
  FOREIGN KEY (EXTRACT_ID) REFERENCES LOCALIZED_TEXT ( "ID" )
 );
 
 CREATE TABLE "SOURCE"
 (
- "ID"          numeric NOT NULL,
- IDENTIFIER    text NOT NULL,
+ "ID"          numeric PRIMARY KEY,
+ IDENTIFIER    text NOT NULL UNIQUE,
  SOURCE_TYPE   text NULL,
  AUTHOR        text NULL,
  PLACE_ID      numeric NULL,
  DATE_ID       numeric NULL,
  REPOSITORY_ID numeric NULL,
  LOCATION      text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (PLACE_ID) REFERENCES HISTORIC_PLACE ( "ID" ),
  FOREIGN KEY (DATE_ID) REFERENCES HISTORIC_DATE ( "ID" ),
  FOREIGN KEY (REPOSITORY_ID) REFERENCES REPOSITORY ( "ID" )
@@ -44,11 +41,10 @@ CREATE TABLE "SOURCE"
 
 CREATE TABLE REPOSITORY
 (
- "ID"       numeric NOT NULL,
- IDENTIFIER text NOT NULL,
+ "ID"       numeric PRIMARY KEY,
+ IDENTIFIER text NOT NULL UNIQUE,
  TYPE       text NULL,
  PERSON_ID  numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (PERSON_ID) REFERENCES PERSON ( "ID" )
 );
 
@@ -57,23 +53,21 @@ CREATE TABLE REPOSITORY
 
 CREATE TABLE HISTORIC_DATE
 (
- "ID"                 numeric NOT NULL,
+ "ID"                 numeric PRIMARY KEY,
  "DATE"               text NOT NULL,
  CALENDAR_ID          numeric NULL,
  DATE_ORIGINAL        text NULL,
  CALENDAR_ORIGINAL_ID numeric NULL,
  CERTAINTY            text NULL,
  CREDIBILITY          numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (CALENDAR_ID) REFERENCES CALENDAR ( "ID" ),
  FOREIGN KEY (CALENDAR_ORIGINAL_ID) REFERENCES CALENDAR ( "ID" )
 );
 
 CREATE TABLE CALENDAR
 (
- "ID" numeric NOT NULL,
- TYPE text NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ "ID" numeric PRIMARY KEY,
+ TYPE text NOT NULL
 );
 
 
@@ -81,8 +75,8 @@ CREATE TABLE CALENDAR
 
 CREATE TABLE PLACE
 (
- "ID"                   numeric NOT NULL,
- IDENTIFIER             text NOT NULL,
+ "ID"                   numeric PRIMARY KEY,
+ IDENTIFIER             text NOT NULL UNIQUE,
  NAME_ID                numeric NOT NULL,
  TYPE                   text NULL,
  COORDINATE             text NULL,
@@ -91,7 +85,6 @@ CREATE TABLE PLACE
  PRIMARY_PLACE_ID       numeric NULL,
  IMAGE_ID               numeric NULL,
  IMAGE_CROP             text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (NAME_ID) REFERENCES LOCALIZED_TEXT ( "ID" ),
  FOREIGN KEY (PRIMARY_PLACE_ID) REFERENCES PLACE ( "ID" ),
  FOREIGN KEY (IMAGE_ID) REFERENCES MEDIA ( "ID" )
@@ -99,11 +92,10 @@ CREATE TABLE PLACE
 
 CREATE TABLE HISTORIC_PLACE
 (
- "ID"        numeric NOT NULL,
+ "ID"        numeric PRIMARY KEY,
  PLACE_ID    numeric NOT NULL,
  CERTAINTY   text NULL,
  CREDIBILITY numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (PLACE_ID) REFERENCES PLACE ( "ID" )
 );
 
@@ -112,25 +104,23 @@ CREATE TABLE HISTORIC_PLACE
 
 CREATE TABLE LOCALIZED_TEXT
 (
- "ID"               numeric NOT NULL,
+ "ID"               numeric PRIMARY KEY,
  TEXT               text NOT NULL,
  LOCALE             text NULL,
  TYPE               text NULL,
  TRANSCRIPTION      text NULL,
  TRANSCRIPTION_TYPE text NULL,
  REFERENCE_TABLE    text NULL,
- REFERENCE_ID       numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ REFERENCE_ID       numeric NULL
 );
 -- REFERENCE: "citation", "person name", "place"
 
 CREATE TABLE NOTE
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  NOTE            text NOT NULL,
  REFERENCE_TABLE text NULL,
- REFERENCE_ID    numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ REFERENCE_ID    numeric NULL
 );
 -- REFERENCE: "project", "assertion", "citation", "source", "cultural norm", "historic date", "calendar", "event", "repository", "historic place", "place", "person name", "person", "group", "research status", "media"
 
@@ -139,21 +129,19 @@ CREATE TABLE NOTE
 
 CREATE TABLE MEDIA
 (
- "ID"             numeric NOT NULL,
- IDENTIFIER       text NOT NULL,
+ "ID"             numeric PRIMARY KEY,
+ IDENTIFIER       text NOT NULL UNIQUE,
  TITLE            text NULL,
  TYPE             text NULL,
- IMAGE_PROJECTION text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ IMAGE_PROJECTION text NULL
 );
 
 CREATE TABLE MEDIA_JUNCTION
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  MEDIA_ID        numeric NOT NULL,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (MEDIA_ID) REFERENCES MEDIA ( "ID" )
 );
 -- REFERENCE: "cultural norm", "event", "repository", "source", "citation", "assertion", "place", "note", "person name", "person", "group", "media", "research status"
@@ -163,21 +151,20 @@ CREATE TABLE MEDIA_JUNCTION
 
 CREATE TABLE PERSON
 (
- "ID"       numeric NOT NULL,
+ "ID"       numeric PRIMARY KEY,
  IMAGE_ID   numeric NULL,
  IMAGE_CROP text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (IMAGE_ID) REFERENCES MEDIA ( "ID" )
 );
 
 CREATE TABLE PERSON_NAME
 (
- "ID"                   numeric NOT NULL,
+ "ID"                   numeric PRIMARY KEY,
  PERSON_ID              numeric NOT NULL,
  NAME_ID                numeric NULL,
  TYPE                   text NULL,
  ALTERNATIVE_SORT_ORDER text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
+ CONSTRAINT PRIMARY KEYPK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (PERSON_ID) REFERENCES PERSON ( "ID" ),
  FOREIGN KEY (NAME_ID) REFERENCES PERSON_NAME ( "ID" )
 );
@@ -187,24 +174,22 @@ CREATE TABLE PERSON_NAME
 
 CREATE TABLE "GROUP"
 (
- "ID"       numeric NOT NULL,
+ "ID"       numeric PRIMARY KEY,
  TYPE       text NULL,
  IMAGE_ID   numeric NULL,
  IMAGE_CROP text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (IMAGE_ID) REFERENCES MEDIA ( "ID" )
 );
 
 CREATE TABLE GROUP_JUNCTION
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  GROUP_ID        numeric NOT NULL,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
  ROLE            text NULL,
  CERTAINTY       text NULL,
  CREDIBILITY     numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (GROUP_ID) REFERENCES "GROUP" ( "ID" )
 );
 -- REFERENCE: "person", "group"
@@ -214,14 +199,13 @@ CREATE TABLE GROUP_JUNCTION
 
 CREATE TABLE EVENT
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  EVENT_TYPE      text NOT NULL,
  DESCRIPTION     text NULL,
  PLACE_ID        numeric NULL,
  DATE_ID         numeric NULL,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (PLACE_ID) REFERENCES HISTORIC_PLACE ( "ID" ),
  FOREIGN KEY (DATE_ID) REFERENCES HISTORIC_DATE ( "ID" )
 );
@@ -232,26 +216,24 @@ CREATE TABLE EVENT
 
 CREATE TABLE CULTURAL_NORM
 (
- "ID"        numeric NOT NULL,
+ "ID"        numeric PRIMARY KEY,
  DESCRIPTION text NOT NULL,
  PLACE_ID    numeric NULL,
  DATE_ID     numeric NULL,
  CERTAINTY   text NULL,
  CREDIBILITY numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (PLACE_ID) REFERENCES HISTORIC_PLACE ( "ID" ),
  FOREIGN KEY (DATE_ID) REFERENCES HISTORIC_DATE ( "ID" )
 );
 
 CREATE TABLE CULTURAL_NORM_JUNCTION
 (
- "ID"             numeric NOT NULL,
+ "ID"             numeric PRIMARY KEY,
  CULTURAL_NORM_ID numeric NOT NULL,
  REFERENCE_TABLE  text NOT NULL,
  REFERENCE_ID     numeric NOT NULL,
  CERTAINTY        text NULL,
  CREDIBILITY      numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (CULTURAL_NORM_ID) REFERENCES CULTURAL_NORM ( "ID" )
 );
 -- REFERENCE: "assertion", "event", "note", "person", "person name", "group"
@@ -259,50 +241,54 @@ CREATE TABLE CULTURAL_NORM_JUNCTION
 
 -- Other application-related things
 
+CREATE TABLE PROJECT
+(
+ "ID"             numeric PRIMARY KEY,
+ PROTOCOL_NAME    text NOT NULL,
+ PROTOCOL_VERSION text NOT NULL,
+ COPYRIGHT        text NULL,
+ NOTE             text NULL
+);
+
 CREATE TABLE RESTRICTION
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
- RESTRICTION     text NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ RESTRICTION     text NOT NULL
 );
 
 CREATE TABLE MODIFICATION
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
  CREATION_DATE   date NOT NULL,
- UPDATE_DATE     date NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ UPDATE_DATE     date NULL
 );
 
 CREATE TABLE RESEARCH_STATUS
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
  DESCRIPTION     text NOT NULL,
  STATUS          text NULL,
- PRIORITY        numeric NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ PRIORITY        numeric NULL
 );
 
 CREATE TABLE CONTACT
 (
- "ID"      numeric NOT NULL,
+ "ID"      numeric PRIMARY KEY,
  CALLER_ID text NOT NULL,
- NOTE      text NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" )
+ NOTE      text NULL
 );
 
 CREATE TABLE CONTACT_JUNCTION
 (
- "ID"            numeric NOT NULL,
+ "ID"            numeric PRIMARY KEY,
  CONTACT_ID      numeric NOT NULL,
  REFERENCE_TABLE text NOT NULL,
  REFERENCE_ID    numeric NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( "ID" ),
  FOREIGN KEY (CONTACT_ID) REFERENCES CONTACT ( "ID" )
 );
